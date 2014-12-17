c0dd63f25b194940b369269e90cada2bd55a8fc4
Milosch Meriac <milosch@meriac.com>
2014-12-17 01:00:30 +0000

    enable red & blue alternate blinking

--- k64f/minimal/minimal.c
+++ k64f/minimal/minimal.c
@@ -10,16 +10,23 @@ void OnReset(void)
 
 	/* enable clock for PORTB */
 	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
-	/* set PORTB pin 21 to ALT1 function (GPIO) */
-	PORTB->PCR[21] = PORT_PCR_MUX(1);
+	/* set PORTB pin 21/22 to ALT1 function (GPIO) */
+	PORTB->PCR[21] = PORT_PCR_MUX(1)|PORT_PCR_DSE_MASK;
+	PORTB->PCR[22] = PORT_PCR_MUX(1)|PORT_PCR_DSE_MASK;
+	/* enable output for PORTB pin 21/22 */
+	PTB->PDDR |= (1UL << 21) | (1UL << 22);
+	/* set PORTB pin 21 */
+	PTB->PSOR |= (1UL << 21);
+	/* clear PORTB pin 22 */
+	PTB->PCOR |= (1UL << 22);
 
-	/* blink forever */
+	/* blink red & blue LED alternating */
 	while(1)
 	{
-		/* toggle PORTB pin 21 ... */
-		PTB->PTOR = 1UL << 21;
+		/* toggle PORTB pin 21 & 22 ... */
+		PTB->PTOR = (1UL << 21)|(1UL << 22);
 		/* ... and wait */
-		for(i=0; i<10000000; i++);
+		for(i=0; i<1000000; i++);
 	}
 }
 
